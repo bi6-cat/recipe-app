259ddaa65d651629c7206dbb42250f4628985fe1
Nguyen Quang <quangnn906@gmail.com>
2024-11-18 19:35:02 +0700

    step 4: add master layout component with template and test suite

---
+++ src/modules/shared/master-layout/master-layout.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-master-layout',
+  standalone: true,
+  imports: [],
+  templateUrl: './master-layout.component.html',
+  styleUrl: './master-layout.component.css'
+})
+export class MasterLayoutComponent {
+
+}
