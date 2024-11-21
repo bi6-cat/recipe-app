d3de06f4cc81e933174cc6076c84d14f52a1c042
Nguyen Quang <quangnn906@gmail.com>
2024-11-21 17:14:03 +0700

    step 7: clean up code structure and improve routing configuration

--- src/modules/app.routes.ts
+++ src/modules/app.routes.ts
@@ -1,3 +1,27 @@
 import { Routes } from '@angular/router';
+import { CustomerLayoutComponent } from './shared/customer-layout/customer-layout.component';
+import { MasterLayoutComponent } from './shared/master-layout/master-layout.component';
+import { AuthLayoutComponent } from './shared/auth-layout/auth-layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    // Auth Page
+    path: 'auth',
+    component: AuthLayoutComponent,
+    loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule),
+  },
+  {
+    // Master Page
+    path: 'master',
+    component: MasterLayoutComponent,
+    loadChildren: () =>
+      import('./master/master.module').then((m) => m.MasterModule),
+  },
+  {
+    // Home Page
+    path: '',
+    component: CustomerLayoutComponent,
+    loadChildren: () =>
+      import('./customer/customer.module').then((m) => m.CustomerModule),
+  },
+];
